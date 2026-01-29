079dfe406f50d6125fe460c3e7bf47a752652357
Ranpul88 <vithanagevinuja@gmail.com>
2026-01-29 22:16:05 +0530

    remove course component ✏️

---
+++ src/app/components/removeCourseButton.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+export default function RemoveCourseButton(props) {
+    
+    const courseName = props.courseName;
+    const [isAvailable, setIsAvailable] = useState(props.availability);
+
+    async function toggleAvailability(){{
+        setIsAvailable(!isAvailable);
+        
+        fetch(process.env.NEXT_PUBLIC_BACKEND_URL + `/admin/courses/${courseName}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ availability: isAvailable })
+        })
+        .then(res => {
+        if(!res.ok){
+            toast.error("Error updating course. Please try again.");
+            return
+        }
+        toast.success("Course updated successfully!");
+        })
+    }}
+
+    return (
+    <>
+        {isAvailable ? <button className="border px-2 py-1 bg-green-600 rounded-md hover:bg-green-500 cursor-pointer" onClick={toggleAvailability}>
+            Restore
+        </button> : <button className="border px-2 py-1 bg-red-600 rounded-md hover:bg-red-500 cursor-pointer" onClick={toggleAvailability}>
+            Remove
+        </button>}
+    </>
+
+  )
+}
+
