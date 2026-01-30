38843e25d7cb8901c5c3cc3eb2591b10c0a6ff7b
Ranpul88 <vithanagevinuja@gmail.com>
2026-01-30 08:38:35 +0530

    course content field in update course page ✏️

--- src/app/admin/courses/update-course/page.jsx
+++ src/app/admin/courses/update-course/page.jsx
@@ -63,6 +63,22 @@ export default function UpdateCourse() {
         const deliveryInArray = delivery.split(',')
         const intakesInArray = intakes.split(',')
 
+        const courseContentPromises = []
+        
+        for(let i = 0; i < files.length; i++){
+            const promises = uploadFile(files[i])
+            courseContentPromises.push(promises)
+        }
+    
+        const courseContent = await Promise.all(courseContentPromises)
+            .catch((err)=>{
+                toast.error("Error uploading course content, Please try again.")
+                console.log("Error uploading course content: ")
+                console.log(err)
+                setIsLoading(false)
+                return
+            })
+
         const res = await fetch( process.env.NEXT_PUBLIC_BACKEND_URL + '/admin/courses/' + courseName, {
         method: 'PUT',
         headers: {
@@ -76,6 +92,7 @@ export default function UpdateCourse() {
             entryRequirements: entryRequirements,
             hallNo: hallNo,
             intakes: intakesInArray,
+            courseContent: courseContent,
             availability: availability
         })
         });
@@ -136,10 +153,10 @@ export default function UpdateCourse() {
                 <label className="flex items-baseline mb-1 font-medium text-gray-700">Intakes<p className="text-sm text-gray-500">(monthes)</p></label>
                 <input type="text" value={intakes} onChange={(e) => {setIntakes(e.target.value), setDisabled(false)}} className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-accent" />
             </div>
-            {/* <div className="flex flex-col">
+            <div className="flex flex-col">
                 <label className="flex items-baseline mb-1 font-medium text-gray-700">Course Content</label>
                 <input type="file" multiple={true} value={files} onChange={(e) => {setFiles(e.target.value), setDisabled(false)}} className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-accent cursor-pointer" />
-            </div> */}
+            </div>
             <div className="flex flex-col">
                 <label className="mb-1 font-medium text-gray-700">Availability</label>
                 <select value={availability} onChange={(e) => {setAvailability(e.target.value), setDisabled(false)}} className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-accent">
