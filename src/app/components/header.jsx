c66c0a09198f34d0ae859b659245cdf96985289c
Ranpul88 <vithanagevinuja@gmail.com>
2026-02-09 18:16:21 +0530

    userData component ✏️

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
+import UserData from "./userData";
 
 export default function Header() {
   return (
-    <div className="w-full h-14 text-accent shadow-xl flex flex-row">
+    <div className="w-full h-14 text-accent shadow-xl flex flex-row ">
         <img src="/logo.png" alt="logo" className="h-14 mt-1 object-cover" />
         <div className="w-full flex items-center justify-center gap-6">
             <Link href='/' className="hover:scale-105">Home</Link>
@@ -10,6 +11,7 @@ export default function Header() {
             <Link href='/about-us' className="hover:scale-105">About Us</Link>
             <Link href='/contact-us' className="hover:scale-105">Contact Us</Link>
         </div>
+        <UserData />
     </div>
   )
 }
